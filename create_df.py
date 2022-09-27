de8d21f6542dbec4641c17b506e897090040ff0a
BrunoKauanL <brunokauanlunardon@gmail.com>
2022-09-27 15:21:14 -0300

    Change the documentation of the make_MultiIndex function.

--- create_df.py
+++ create_df.py
@@ -1,21 +1,30 @@
 import pandas as pd
 import numpy as np
 
-dic={"album1":{"musicas":["musica11", "musica21"], "duração":[0.55,0.65]}, "album2":{"musicas":["musica12", "musica22"], "duração":[0.55,0.65]}}
+dic={"album1":{"musics":["music11", "music21"], "duration":[0.55,0.65], 
+                "views":[1250,1200]},
+     "album2":{"musics":["music12", "music22"], "duration":[0.75,0.85], 
+                "views":[1150,1100]}}
 
 def make_MultiIndex(dic):
-    """Essa função cria um objeto MultiIndex para ser usado nos dataframes que serão analisados.
+    """Creates a pandas MultiIndex object that will be used for further indexation of
+    the pandas DataFrame to be analyzed.
 
-    :param dic: Dicionário com os dados obtidos no web-scrapping
-    :type dic: dict
-    :return: Objeto MultiIndex do pandas
+    :param dic: A dictionary of dictionaries, where the keys of the most external 
+    dictionary are the albums and its values are dictionaries. In the internal dictionaries, 
+    the keys of each one of them are the columns or indexes of the pandas DataFrame to be built.
+    :type dic: dict.
+    :return: Pandas MultiIndex object
     :rtype: pandas.core.indexes.multi.MultiIndex
     """
-    lista=[]
+    array=[]
     for key in dic.keys():
-        for e in dic[key]["musicas"]:
-            lista.append(list([key,e]))
-    df=pd.DataFrame(lista,columns=["albuns","musicas"])
+        for elements in dic[key]["musics"]:
+            array.append(list([key,elements]))
+    df=pd.DataFrame(array,columns=["albums","musics"])
     multi_index=pd.MultiIndex.from_frame(df)
     return multi_index
+
+
+
 
