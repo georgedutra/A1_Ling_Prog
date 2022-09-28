88b835daec4f0f8c3f188dd374715f59d4dd9104
BrunoKauanL <brunokauanlunardon@gmail.com>
2022-09-27 23:17:06 -0300

    Create the function song_duration_album

--- questions.py
+++ questions.py
@@ -13,7 +13,7 @@ sns.set_theme(context="paper", style="darkgrid")
 
 def song_popularity_album(dic):
     """Create a bar chart of each album to answer the question "which songs are most 
-    listened and least listened per album".
+    listened and least listened per album?".
 
     :param dic: Dictionary
     :type dic: dict
@@ -45,10 +45,52 @@ def song_popularity_album(dic):
         plt.title(label=f"{album}", loc="center", pad=10)
 
         #Footnote.
-        plt.figtext(0.1, 0.01, f"""The most popular music of {album} is 
+        plt.figtext(0.1, 0.01, f"""The most popular song of {album} is 
 {most_popular} and the least is {least_popular}.""", ha="left", fontsize=8, 
 bbox={"facecolor": "white", "pad": -10})
 
         #Save and close the plot.
-        plt.savefig(f"imgs/fig{album}.png")
+        plt.savefig(f"imgs/{album}_popularity.png")
+        plt.close()
+
+def song_duration_album(dic):
+    """Create a bar chart of each album to answer the question "which songs are longest 
+    and which are shortest per album?".
+
+    :param dic: Dictionary
+    :type dic: dict
+    """    
+    df=make_df(dic)
+    
+    #A numpy array with the values of the first level of MultiIndex.
+    albums=np.unique(make_MultiIndex(dic).get_level_values("Album"))
+
+    for album in albums:
+        #Select the row of the DataFrame related to one specific album.
+        df_sliced=df.xs(album)
+        
+        #The index of the longest song (wich is the name of the song and the 
+        #second level of MultiIndex).
+        longest=df_sliced["duration"].astype(float).idxmax()
+        
+        #The index of the shortest song (wich is the name of the song and the 
+        #second level of MultiIndex).
+        shortest=df_sliced["duration"].astype(float).idxmin()
+
+        #Seaborn to make the visualization.
+        plot=sns.barplot(data=df_sliced, x=df_sliced.index, y="duration")
+
+        #Y-axis label
+        plot.set(ylabel="Duration")
+
+        #Title.
+        plt.title(label=f"{album}", loc="center", pad=10)
+
+        #Footnote.
+        plt.figtext(0.1, 0.01, f"""The longest song of {album} is 
+{longest} and the shortest is {shortest}.""", ha="left", fontsize=8, 
+bbox={"facecolor": "white", "pad": -10})
+
+        #Save and close the plot.
+        plt.savefig(f"imgs/{album}_duration.png")
         plt.close()
