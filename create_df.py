4674a9d44179181eea7d87d502bc0ba3459e8365
BrunoKauanL <brunokauanlunardon@gmail.com>
2022-10-03 04:19:28 -0300

    Create ms_to_min_sec and Update make_df

--- create_df.py
+++ create_df.py
@@ -1,5 +1,32 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 import pandas as pd
 from spotify_api import get_tracks_data
+from mus_br_letras import get_nbhd_songs
+
+def ms_to_min_sec(ms):
+    #Convert miliseconds to minutes.
+    minutes = ms / 1000 / 60
+
+    #Get the rest of the previous division.
+    seconds = ms / 1000 % 60
+
+    #Convert the minutes and the seconds to int (would be strange to return something
+    #like 1.67:45.6, min:sec).
+    seconds=int(seconds)
+    minutes=int(minutes)
+
+    decimal=list()
+    for i in range(0,10):
+        decimal.append(i)
+  
+    #If seconds are less than 10 the return would be something like 4:6 is better if 
+    #it's 4:06.  
+    if seconds in decimal:
+        return f"{minutes}:0{seconds}"
+    else:
+        return f"{minutes}:{seconds}"
 
 
 def make_MultiIndex(dic):
@@ -22,6 +49,7 @@ def make_MultiIndex(dic):
     return multi_index
 
 def make_df(dic):
+    #ConectionError
     """Creates the pandas DataFrame that will be used for all data analysis.
 
     :param dic: A dictionary of dictionaries, where the keys of the most external 
@@ -31,20 +59,35 @@ def make_df(dic):
     :return: Pandas DataFrame object
     :rtype: pandas.core.frame.DataFrame
     """    
-    #get an arbitrary key of the main dictionary
+    #get an arbitrary key of the main dictionary.
     main_dic_key=list(dic.keys())[0]
+
     #get all the keys of an arbitrary internal dictionary 
-    #(the sets of keys of any internal dictionary are the same)
+    #(the sets of keys of any internal dictionary are the same).
     sub_dic_keys=list(dic[main_dic_key].keys())
+
     #Creates an empty DataFrame with the internal dictionary keys as columns, 
-    #(except the "Music" key that will be part of the MultiIndex)
+    #(except the "Music" key that will be part of the MultiIndex).
     df=pd.DataFrame(columns=sub_dic_keys).drop("tracks_names", axis=1)
+
+    #Concatenates all individual DFs of each album into one DF with a MultiIndex.
     for key in dic.keys():
         data=dic[key]
         partial_df=pd.DataFrame(data).drop("tracks_names", axis=1)
         df=pd.concat([df,partial_df])
-    final_df=df.set_index(make_MultiIndex(dic))
-    final_df.to_csv("final_df.csv")
-    return final_df
+
+    #Handling the DF before save as csv.   
+    df_spotify=df.set_index(make_MultiIndex(dic))
+    df_spotify=df_spotify.drop(labels=['Female Robbery', 'Fallen Star','Middle of Somewhere',
+    'Spotify Sessions','Sweater Weather (Young Saab Remix)','Yellow Box',"Daddy Issues (Remix) feat. Syd",
+    "Thank You,"], level='Album')
+    df_spotify['tracks_duration_ms']=df_spotify['tracks_duration_ms'].apply(ms_to_min_sec)
+    df_spotify.rename(columns={'tracks_duration_ms':'tracks_duration'}, inplace=True)
+
+    #Save
+    df_spotify.to_csv("final_df.csv")
+
+    #If needed to use the DF directly.
+    return df_spotify
 
 make_df(get_tracks_data())
