fb2ab88a794803bfe75c7e0940cf2d7f0020aa62
Zuilho Segundo <segundozuilho@gmail.com>
2022-10-08 18:16:52 -0300

    update main

--- main.py
+++ main.py
@@ -9,5 +9,13 @@ import group_2
 
 df_tnbh = pd.read_csv("TNBH_Data.csv", index_col=[0,1])
 
-print(df_tnbh.keys())
+print("Group Question 2")
+print("================")
+print("Question 1:")
+group_2.question_1(df_tnbh)
 
+print("Question 2:")
+group_2.question_2(df_tnbh)
+
+print("Question 3:")
+group_2.question_3(df_tnbh)
