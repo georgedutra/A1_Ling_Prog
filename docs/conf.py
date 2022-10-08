9bac1de9d04d6317a04d96d1c104594c346f0a85
Zuilho Segundo <segundozuilho@gmail.com>
2022-10-08 15:51:08 -0300

    update conf

--- docs/conf.py
+++ docs/conf.py
@@ -139,5 +139,5 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-autodoc_mock_imports = ['numpy', 'pandas', 'wordcloud', 'multidict']
+autodoc_mock_imports = ['numpy', 'pandas', 'wordcloud', 'multidict', 'bs4']
 
